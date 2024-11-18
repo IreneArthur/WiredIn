51310c4c5e641caaf622cb018ee32768abfbd3a3
Arthur <shingiroirene@gmail.com>
2024-11-18 14:04:57 +0200

    added functions to handle diiferent tasks for inputs display and getting results

--- Rock-Paper-Scissors/rock-paper-scissor.py
+++ Rock-Paper-Scissors/rock-paper-scissor.py
@@ -1,30 +1,44 @@
 import random
 
-print('Rock-Paper-Scissors Start!')
+def start_message():
+    print('Rock-Paper-Scissors Start!')
 
-# Prompt the user to input their choice
-print('Enter your hand:')
-my_hand = int(input('0: Rock, 1: Scissors, 2: Paper: '))
-you_hand = random.randint(0, 2)
+def get_player():
+    print('Enter your hand:')
+    my_hand = int(input('0: Rock, 1: Paper, 2: Scissors: '))
+    return my_hand
 
-if my_hand == 0:
-    if you_hand == 0:
-        print('It\'s a tie!')
-    elif you_hand == 1:
-        print('You win!')
-    elif you_hand == 2:
-        print('You Lose!')
-elif my_hand == 1:
-    if you_hand == 0:
+def get_computer():
+    comp_hand = random.randint(0, 2)
+    return comp_hand
+
+def view_result(hand_diff):
+    if (hand_diff == -1 or hand_diff == 2):
         print('You lose!')
-    elif you_hand == 1:
-        print('It\'s a tie!')
-    elif you_hand == 2:
-        print('You win!')
-elif my_hand == 2:
-    if you_hand == 0:
-        print('You win!')
-    elif you_hand == 1:
-        print('You Lose!')
-    elif you_hand == 2:
-        print('It\'s a tie!')
+    elif(hand_diff == 1 or hand_diff == -2):
+        print('You Win!')
+    elif hand_diff == 0 :
+        print('it\'s a Draw')
+
+def get_hand_name(hand_number):
+    if hand_number == 0:
+        return 'Rock'
+    elif hand_number == 1:
+        return 'Paper'
+    elif hand_number == 2:
+        return 'Scissors'
+    else:
+        return 'unknown'
+    return choice
+
+def view_hand(player, computer):
+    playerChoice = get_hand_name(player)
+    compChoice = get_hand_name(computer)
+    print(f"Your choice is {playerChoice} and the computer\'s choice is {compChoice} so")
+
+start_message()
+playersHand = get_player()
+computersHand = get_computer()
+hand_diff = playersHand - computersHand
+view_hand(playersHand, computersHand)
+view_result(hand_diff)
